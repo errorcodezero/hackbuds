0ef2712072358d6e26fe445dda51eac600c9aa81
ErrorCode0 <me@errorcodezero.dev>
2025-05-16 11:00:16 -0700

    adding catppuccin styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/state';
-	import { redirect } from '@sveltejs/kit';
-	import { SignIn } from '@auth/sveltekit/components';
-
-	console.log(page.data);
-	console.log(page.data.session);
-
-	// if (page.data.session) {
-	// 	redirect(302, '/home');
-	// }
 </script>
 
 <h1>Hackbuds</h1>
