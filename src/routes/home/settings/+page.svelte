155dbaeb9ccc10a67e822dcfeca8ac688286318b
ErrorCode0 <me@errorcodezero.dev>
2025-05-23 14:51:44 -0700

    settings edit page

--- src/routes/home/settings/+page.svelte
+++ src/routes/home/settings/+page.svelte
@@ -1,17 +1,47 @@
 <script>
 	import Button from '$lib/components/Button.svelte';
+	import TextInput from '$lib/components/TextInput.svelte';
 	import { signOut } from '@auth/sveltekit/client';
+
+	let description = $state('');
 </script>
 
 <div class="container">
 	<h1>Settings</h1>
-	<Button
-		type="danger"
-		onclick={() => {
-			fetch('/api/deleteUser', { method: 'DELETE' });
-			signOut();
-		}}>Delete account</Button
-	>
+	<div class="edit">
+		<Button
+			type="danger"
+			onclick={() => {
+				fetch('/api/deleteUser', { method: 'DELETE' });
+				signOut();
+			}}>Delete account</Button
+		>
+		<!-- TODO: make an actual settings edit page -->
+		<form
+			onsubmit={(event) => {
+				event.preventDefault();
+			}}
+		>
+			<TextInput placeholder="Description" type="text" value={description} />
+			<Button onclick={null} type="primary">Change description</Button>
+		</form>
+		<form
+			onsubmit={(event) => {
+				event.preventDefault();
+			}}
+		>
+			<TextInput placeholder="Tags(space between each)" type="text" value={description} />
+			<Button onclick={null} type="primary">Change tags</Button>
+		</form>
+		<form
+			onsubmit={(event) => {
+				event.preventDefault();
+			}}
+		>
+			<TextInput placeholder="Colors(space between each)" type="text" value={description} />
+			<Button onclick={null} type="primary">Change colors</Button>
+		</form>
+	</div>
 </div>
 
 <style>
